468a778f1e53566795decce9ae5e5c5cd1130054
Alex Frol <drbo19777@gmail.com>
2023-01-23 21:56:57 +0300

    Init commit

---
+++ Task_1/Task_1/Task_1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+void num_fibonach(int a, int b, int num) {
+	int temp = a + b;
+	if (a == 1 && b == 1) {
+		std::cout << a << " " << b << " ";
+	}
+	b = a;
+	a = temp;
+	if (temp < num) {
+		std::cout << temp << " ";
+		num_fibonach(a, b, num);
+	}
+	else {
+		std::cout << std::endl;
+	}
+}
+
+
+int main(int argc, char** argv) {
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	int	a = 1,
+		b = 1,
+		num = 0;
+
+	std::cout << "Введите число: ";
+	std::cin >> num;
+
+	num_fibonach(a, b, num);
+
+	return 0;
+}
---
+++ Task_2/Task_2/Task_2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <vector>
+
+void num_fibonach(int num, std::vector <int>& vec_num) {
+	int temp = vec_num[vec_num.size() - 2] + vec_num[vec_num.size() - 1];
+	if (temp < num) {
+		vec_num.push_back(temp);
+		num_fibonach(num, vec_num);
+	}
+}
+
+int main(int argc, char** argv) {
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int num;
+	std::vector <int> vec_num = { 1, 1 };
+
+	std::cout << "Введите число: ";
+	std::cin >> num;
+
+	num_fibonach(num, vec_num);
+
+	for (int i : vec_num) {
+		std::cout << i << " ";
+	}
+	std::cout << std::endl;
+
+	return 0;
+}
