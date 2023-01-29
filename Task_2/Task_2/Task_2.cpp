84a738da229a0622f758b411630c3b109419cd5d
Alex Frol <drbo19777@gmail.com>
2023-01-29 13:20:41 +0300

    Changed Task_2

--- Task_2/Task_2/Task_2.cpp
+++ Task_2/Task_2/Task_2.cpp
@@ -2,12 +2,22 @@
 #include <Windows.h>
 #include <vector>
 
-void num_fibonach(int num, std::vector <int>& vec_num) {
+int num_fibonach(int num, std::vector <int>& vec_num) {
 	int temp = vec_num[vec_num.size() - 2] + vec_num[vec_num.size() - 1];
 	if (temp < num) {
 		vec_num.push_back(temp);
 		num_fibonach(num, vec_num);
 	}
+	else {
+		return vec_num.back();
+	}
+}
+void print(std::vector <int> vec_num, int num_back) {
+	for (int i : vec_num) {
+		std::cout << i << " ";
+	}
+	std::cout << std::endl;
+	std::cout << "Последнее рассчитанное число: " << num_back << std::endl;
 }
 
 int main(int argc, char** argv) {
@@ -21,12 +31,8 @@ int main(int argc, char** argv) {
 	std::cout << "Введите число: ";
 	std::cin >> num;
 
-	num_fibonach(num, vec_num);
-
-	for (int i : vec_num) {
-		std::cout << i << " ";
-	}
-	std::cout << std::endl;
+	int num_back = num_fibonach(num, vec_num);
+	print(vec_num, num_back);
 
 	return 0;
 }
